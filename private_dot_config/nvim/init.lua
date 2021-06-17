a668bd9ba94aaea415197b7e22cc8495f604ae2a
Brandon Duval Benn <me@brandonbenn.xyz>
2021-06-17 08:42:53 +0800

    Update private_dot_config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,5 +1,4 @@
 local cmd, fn, g, opt = vim.cmd, vim.fn, vim.g, vim.opt
-local function exec(command) vim.api.nvim_exec(command, false) end
 local function map(mode, lhs, rhs, opts)
   local options = {noremap = true}
   if opts then options = vim.tbl_extend('force', options, opts) end
@@ -15,16 +14,15 @@ opt.nu  = true
 opt.acd = true
 opt.rnu = true
 opt.udf = true
-
-cmd 'command! Buffers Telescope buffers'
-cmd 'command! Finder  Telescope file_browser'
-cmd 'command! Files   Telescope find_files'
-cmd 'command! Grep    Telescope live_grep'
-
+cmd('command! Buffers Telescope buffers')
+cmd('command! Finder  Telescope file_browser')
+cmd('command! Files   Telescope find_files')
+cmd('command! Grep    Telescope live_grep')
+cmd([[autocmd BufWritePre * :%s/\s\+$//e]])
 map('n', 'L', '$')
 map('n', 'H', '^')
-map('i', '<leader>j', '<ESC>')
-map('n', '<leader><leader>', ':update<cr>')
+map('i', 'jj', '<ESC>')
+map('n', '<leader>w', ':update<cr>')
 map('n', '<leader>bd', ':bd<cr>')
 map('n', '<leader>noh', ':noh<cr>')
 map('n', '<leader>q', ':q<cr>')
@@ -35,5 +33,3 @@ map('n', '<leader>y', '"+y')
 map('n', '<leader>Y', 'gg"+yG')
 map('n', '<leader>p', '"+p')
 map('x', 'ga', '<Plug>(EasyAlign)')
-
-exec([[ autocmd BufWritePre * :%s/\s\+$//e ]])
