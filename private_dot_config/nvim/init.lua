f564ea353aeaf490b2b471f8c8574aa3da58a7d5
Brandon Duval Benn <me@brandonbenn.xyz>
2021-11-09 20:03:26 +0800

    Update private_dot_config/nvim/init.lua Update private_dot_local/private_share/buku/bookmarks.db

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,4 +1,4 @@
--- Editor
+-- VANILLA VIM CONFIGURATION
 vim.g.mapleader = ' '
 vim.o.showmode = false
 vim.o.termguicolors = true
@@ -37,105 +37,118 @@ vim.cmd [[vnoremap < <gv]]
 vim.cmd [[vnoremap > >gv]]
 vim.cmd [[autocmd TermOpen * setlocal nonumber norelativenumber]]
 vim.cmd [[tnoremap <Esc> <C-\><C-n>]]
--- Editor
 
-
--- Paq Installation
+-- PLUGIN MANAGER INSTALLATION
 local fn = vim.fn
-local install_path = fn.stdpath('data') .. '/site/pack/paqs/start/paq-nvim'
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', install_path})
+    packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
--- Paq Installation
-
 
 -- Plugins
-require "paq" {
-    "savq/paq-nvim";
-    "nvim-lua/plenary.nvim";
-    "tami5/sqlite.lua"; 
-    "neovim/nvim-lspconfig";
-    {url="https://gitlab.com/th3lusive/typography.vim"};
-    "ap/vim-buftabline";
-    "sbdchd/neoformat";
-    "mcchrish/nnn.vim";
-    "nvim-telescope/telescope.nvim";
-    "tpope/vim-fugitive";
-    "nvim-telescope/telescope-frecency.nvim";
-    {"nvim-telescope/telescope-fzf-native.nvim", run = "make" };
-    "machakann/vim-sandwich";
-    "junegunn/vim-easy-align";
-    "tpope/vim-commentary";
-    "tpope/vim-dispatch";
-    "tpope/vim-endwise";
-    "tpope/vim-eunuch";
-    "pbrisbin/vim-mkdir";
-    "jiangmiao/auto-pairs";
-    "APZelos/blamer.nvim";
-    {"ms-jpq/coq_nvim", branch="coq", 
-    run = function() 
-        require("coq").deps()
-    end};
-    {"ms-jpq/coq.artifacts", branch="artifacts"};
-    "nvim-lua/popup.nvim";
-    "numtostr/FTerm.nvim";
-    "ziglang/zig.vim";
-    {"ray-x/go.nvim",
-    run = function() require('go').setup({
-	    goimport = 'gopls', gofmt = 'gopls',
-    }) end};
-}
+return require('packer').startup(
+function(use)
+    use 'wbthomason/packer.nvim' -- Packer can manage itself
+    use 'nvim-lua/plenary.nvim'  -- Dependency of many plugins
 
-vim.cmd [[color typograph | set background=light]]
-vim.cmd [[nnoremap <silent><C-p> :Telescope find_files theme=ivy<cr>]]
-vim.cmd [[nnoremap <silent><A-h> :Telescope oldfiles theme=dropdown<cr>]]
-vim.cmd [[nnoremap <silent><A-g> :Telescope grep_string theme=dropdown<cr>]]
-vim.cmd [[nnoremap <silent>;f :Neoformat<cr>]]
-vim.cmd [[xnoremap <silent>ga :EasyAlign<cr>]]
-vim.cmd [[nnoremap <silent>- :NnnPicker %:p:h<cr>]]
-vim.cmd [[command! Term lua require('FTerm').toggle()<CR>]] 
-vim.cmd [[autocmd InsertEnter * lua require("coq").Now("--shut-up")]]
-require("nnn").setup({
-    replace_netrw = 1,
-    layout = { window = { width = 0.3, height = 0.4 } }
-})
+    use 'machakann/vim-sandwich'
+    use 'jiangmiao/auto-pairs'
+    use 'tpope/vim-commentary'
+    use 'APZelos/blamer.nvim'
+    use 'nvim-lua/popup.nvim'
+    use 'pbrisbin/vim-mkdir'
+    use 'tpope/vim-dispatch'
+    use 'ap/vim-buftabline'
+    use 'tpope/vim-endwise'
+    use 'tpope/vim-eunuch'
+    use 'ziglang/zig.vim'
 
-require"telescope".load_extension('fzf')
-if pcall(require"telescope".load_extension("frecency")) then
-    vim.cmd [[nnoremap <silent><A-h> :Telescope frecency theme=dropdown<cr>]]
-end
--- Plugins
-
--- LSP Configuration
-local servers = {'gopls', 'zls', 'pyright','clangd', 'solargraph'}
-local lsp = require 'lspconfig'
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities.textDocument.completion.completionItem.resolveSupport = {
-    properties = {
-        'documentation',
-        'detail',
-        'additionalTextEdits',
+    use {
+        'https://gitlab.com/th3lusive/typography.vim',
+        config = function() vim.cmd 'colorscheme typograph' end
     }
-}
-
-for _, server in ipairs(servers) do
-    lsp[server].setup {
-        settings = {documentFormatting = true},
-        capabilities = capabilities,
-        on_attach = on_attach
+    use {
+        'sbdchd/neoformat', cmd = 'Neoformat',
+        config = function()
+            vim.cmd 'nnoremap <silent><leader> :Neoformat<cr>'
+        end
     }
-end
+    use {
+        'nvim-telescope/telescope.nvim',
+        config = function()
+            vim.cmd 'nnoremap <silent><C-p> :Telescope find_files theme=ivy<cr>'
+            vim.cmd 'nnoremap <silent><A-h> :Telescope oldfiles theme=dropdown<cr>'
+            vim.cmd 'nnoremap <silent><A-g> :Telescope grep_string theme=dropdown<cr>'
+        end,
+        requires = {
+            'nvim-telescope/telescope-fzf-native.nvim', run = 'make',
+            config = function() require'telescope'.load_extension('fzf') end
+        }
+    }
+    use {
+        'junegunn/vim-easy-align', cmd = 'EasyAlign',
+        config = function()
+            vim.cmd 'xnoremap <silent>ga :EasyAlign<cr>'
+        end
+    }
+    use {
+        'mcchrish/nnn.vim',
+        cmd = 'NnnPicker',
+        config = function()
+            vim.cmd 'nnoremap <silent>- :NnnPicker %:p:h<cr>'
+            require("nnn").setup({
+                replace_netrw = 1,
+                layout = { window = { width = 0.3, height = 0.4 } }
+            })
+        end
+    }
+    use {
+        'ms-jpq/coq_nvim', branch = 'coq',
+        requires = {'ms-jpq/coq.artifacts', branch = 'artifacts'},
+        run = function() require'coq'.deps() end,
+        config = function() require 'coq'.Now('-s') end
+    }
+    use {
+        'neovim/nvim-lspconfig',
+        ft = {'go', 'c', 'cpp', 'zig', 'ruby', 'python'},
+        config = function()
+            local servers = {'gopls', 'zls', 'pyright','clangd', 'solargraph'}
+            local lsp = require 'lspconfig'
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities.textDocument.completion.completionItem.snippetSupport = true
+            capabilities.textDocument.completion.completionItem.resolveSupport = {
+                properties = {
+                    'documentation',
+                    'detail',
+                    'additionalTextEdits',
+                }
+            }
 
-vim.cmd [[nnoremap <silent><F2>  :lua vim.lsp.buf.rename()<CR>]]
-vim.cmd [[nnoremap <silent>gd    :lua vim.lsp.buf.declaration()<CR>]]
-vim.cmd [[noremap  <silent><c-]> :lua vim.lsp.buf.definition()<CR>]]
-vim.cmd [[nnoremap <silent>K     :lua vim.lsp.buf.hover()<CR>]]
-vim.cmd [[nnoremap <silent>gD    :lua vim.lsp.buf.implementation()<CR>]]
-vim.cmd [[noremap  <silent><c-k> :lua vim.lsp.buf.signature_help()<CR>]]
-vim.cmd [[nnoremap <silent>1gD   :lua vim.lsp.buf.type_definition()<CR>]]
-vim.cmd [[nnoremap <silent>gr    :lua vim.lsp.buf.references()<CR>]]
-vim.cmd [[nnoremap <silent>g0    :lua vim.lsp.buf.document_symbol()<CR>]]
-vim.cmd [[nnoremap <silent>gW    :lua vim.lsp.buf.workspace_symbol()<CR>]]
-vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()' ]])
--- LSP Configuration
+            for _, server in ipairs(servers) do
+                lsp[server].setup {
+                    settings = {documentFormatting = true},
+                    capabilities = capabilities,
+                    on_attach = on_attach
+                }
+            end
+
+            vim.cmd 'nnoremap <silent><F2>  :lua vim.lsp.buf.rename()<CR>'
+            vim.cmd 'nnoremap <silent>gd    :lua vim.lsp.buf.declaration()<CR>'
+            vim.cmd 'noremap  <silent><c-]> :lua vim.lsp.buf.definition()<CR>'
+            vim.cmd 'nnoremap <silent>K     :lua vim.lsp.buf.hover()<CR>'
+            vim.cmd 'nnoremap <silent>gD    :lua vim.lsp.buf.implementation()<CR>'
+            vim.cmd 'nnoremap <silent><c-k> :lua vim.lsp.buf.signature_help()<CR>'
+            vim.cmd 'nnoremap <silent>1gD   :lua vim.lsp.buf.type_definition()<CR>'
+            vim.cmd 'nnoremap <silent>gr    :lua vim.lsp.buf.references()<CR>'
+            vim.cmd 'nnoremap <silent>g0    :lua vim.lsp.buf.document_symbol()<CR>'
+            vim.cmd 'nnoremap <silent>gW    :lua vim.lsp.buf.workspace_symbol()<CR>'
+            vim.cmd 'command! Format execute "lua vim.lsp.buf.formatting()"'
+        end
+    }
+
+    -- Automatically set up configuration after cloning packer.nvim
+    if packer_bootstrap then
+        require('packer').sync()
+    end
+end)
+
