26c523bf0325f37d0c0a44a64d493b99be615a93
Brandon Duval Benn <me@brandonbenn.xyz>
2021-12-14 18:54:17 +0800

    Update private_dot_config/nvim/lua/utils.lua

--- private_dot_config/nvim/lua/utils.lua
+++ private_dot_config/nvim/lua/utils.lua
@@ -1,39 +1,35 @@
--- Better Keymaps
-local M = {}
-
 -- :help map-modes
-function M.map(mode, lhs, rhs)
+function map(mode, lhs, rhs)
     vim.api.nvim_set_keymap(mode, lhs, rhs, { silent = true })
 end
 
-function M.noremap(mode, lhs, rhs)
+function noremap(mode, lhs, rhs)
     vim.api.nvim_set_keymap(mode, lhs, rhs, { noremap = true, silent = true })
 end
 
-function M.exprnoremap(mode, lhs, rhs)
+function exprnoremap(mode, lhs, rhs)
     vim.api.nvim_set_keymap(mode, lhs, rhs, { noremap = true, silent = true, expr = true })
 end
 
-function M.autocmd(c)
+function autocmd(c)
     vim.cmd("autocmd " .. c)
 end
 
 -- stylua: ignore start
-function M.nmap(lhs, rhs) M.map('n', lhs, rhs) end
+function nmap(lhs, rhs) map('n', lhs, rhs) end
 
-function M.nnoremap(lhs, rhs) M.noremap('n', lhs, rhs) end
+function nnoremap(lhs, rhs) noremap('n', lhs, rhs) end
 
-function M.vnoremap(lhs, rhs) M.noremap('v', lhs, rhs) end
+function vnoremap(lhs, rhs) noremap('v', lhs, rhs) end
 
-function M.xnoremap(lhs, rhs) M.noremap('x', lhs, rhs) end
+function xnoremap(lhs, rhs) noremap('x', lhs, rhs) end
 
-function M.inoremap(lhs, rhs) M.noremap('i', lhs, rhs) end
+function inoremap(lhs, rhs) noremap('i', lhs, rhs) end
 
-function M.tnoremap(lhs, rhs) M.noremap('t', lhs, rhs) end
+function tnoremap(lhs, rhs) noremap('t', lhs, rhs) end
 
-function M.exprinoremap(lhs, rhs) M.exprnoremap('i', lhs, rhs) end
+function exprinoremap(lhs, rhs) exprnoremap('i', lhs, rhs) end
 
-function M.exprnnoremap(lhs, rhs) M.exprnoremap('n', lhs, rhs) end
+function exprnnoremap(lhs, rhs) exprnoremap('n', lhs, rhs) end
 
-return M
 -- stylua: ignore end
