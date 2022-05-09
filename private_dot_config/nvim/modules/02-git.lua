986953446828b0188ce29bc45f9f87f31ba92d10
Brandon Duval Benn <me@brandonbenn.xyz>
2022-05-09 08:16:24 +0800

    Update private_dot_config/nvim/modules/02-git.lua

--- private_dot_config/nvim/modules/02-git.lua
+++ private_dot_config/nvim/modules/02-git.lua
@@ -3,3 +3,9 @@ local gitsigns = require("gitsigns")
 gitsigns.setup()
 
 vim.keymap.set("n", "<leader>gb", gitsigns.toggle_current_line_blame, { silent = true })
+
+if vim.env.WEZTERM_PANE == "0" then
+	vim.keymap.set("n", "<leader>gg", function()
+		vim.fn.system("wezterm cli spawn --cwd " .. vim.fn.getcwd() .. " -- gitui")
+	end, { silent = true })
+end
