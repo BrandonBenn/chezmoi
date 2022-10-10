edbfaa8f74078ed94be6daea8b77db51cdd10505
Brandon Duval Benn <me@brandonbenn.xyz>
2022-10-10 19:15:07 +0800

    Update private_dot_config/nvim/modules/05-terminal.lua

--- private_dot_config/nvim/modules/05-terminal.lua
+++ private_dot_config/nvim/modules/05-terminal.lua
@@ -1,22 +1,20 @@
-vim.defer_fn(function()
-	local autocmd = vim.api.nvim_create_autocmd
-	local command = vim.api.nvim_create_user_command
-	local keymap = vim.keymap.set
+local autocmd = vim.api.nvim_create_autocmd
+local command = vim.api.nvim_create_user_command
+local keymap = vim.keymap.set
 
-	local set_terminal_keymaps = function()
-		local opts = { noremap = true, buffer = 0 }
-		keymap("t", "<esc>", [[<C-\><C-n>]], opts)
-		keymap("t", "<C-W>h", [[<C-\><C-n><C-W>h]], opts)
-		keymap("t", "<C-W>j", [[<C-\><C-n><C-W>j]], opts)
-		keymap("t", "<C-W>k", [[<C-\><C-n><C-W>k]], opts)
-		keymap("t", "<C-W>l", [[<C-\><C-n><C-W>l]], opts)
-	end
-	autocmd("TermOpen", { pattern = { "term://*" }, callback = set_terminal_keymaps })
+local set_terminal_keymaps = function()
+	local opts = { noremap = true, buffer = 0 }
+	keymap("t", "<esc>", [[<C-\><C-n>]], opts)
+	keymap("t", "<C-W>h", [[<C-\><C-n><C-W>h]], opts)
+	keymap("t", "<C-W>j", [[<C-\><C-n><C-W>j]], opts)
+	keymap("t", "<C-W>k", [[<C-\><C-n><C-W>k]], opts)
+	keymap("t", "<C-W>l", [[<C-\><C-n><C-W>l]], opts)
+end
+autocmd("TermOpen", { pattern = { "term://*" }, callback = set_terminal_keymaps })
 
-	local keys = { "z", "x", "c", "v" }
-	for k, v in pairs(keys) do
-		keymap({ "n" }, "m" .. v, function()
-			require("harpoon.term").gotoTerminal(k)
-		end)
-	end
-end, 1000)
+local keys = { "z", "x", "c", "v" }
+for k, v in pairs(keys) do
+	keymap({ "n" }, "m" .. v, function()
+		require("harpoon.term").gotoTerminal(k)
+	end)
+end
