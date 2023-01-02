117370e0764d36dd064afbee910fea9d7f571d28
Brandon Duval Benn <me@brandonbenn.xyz>
2023-01-02 22:46:45 +0800

    Add .config/nvim/init.lua

---
+++ private_dot_config/nvim/init.lua
@@ -0,0 +1,23 @@
+-- Set Options
+vim.cmd.colorscheme("typograph")
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+vim.o.exrc = true
+vim.o.hidden = true
+vim.o.laststatus = 0
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.scrolloff = 999
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.udir = "/tmp/nvim/undo"
+vim.o.undofile = true
+vim.o.wrap = false
+
+-- Set Keymaps
+vim.keymap.set("v", "<", "<gv", { remap = true })
+vim.keymap.set("v", ">", ">gv", { remap = true })
+vim.keymap.set({ "v", "n" }, ";", ":", { remap = true })
+vim.keymap.set({ "n", "v" }, "g=", vim.lsp.buf.format, { silent = true })
+
+require("plugins")
