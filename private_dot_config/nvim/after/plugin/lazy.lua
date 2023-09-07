c90a34f871659ad6d6f08810e3dfc78449f47c40
Brandon Duval Benn <me@brandonbenn.xyz>
2023-09-07 14:48:11 +0800

    Update .config/nvim/after/plugin/lazy.lua Update .config/nvim/init.lua

--- private_dot_config/nvim/after/plugin/lazy.lua
+++ private_dot_config/nvim/after/plugin/lazy.lua
@@ -1,88 +1,84 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	return
+  return
 end
 vim.opt.rtp:prepend(lazypath)
 local lazy = require("lazy")
 
-local opts = {
-	install = { missing = true, colorscheme = { "typograph" } },
-}
+local opts = { install = { missing = true, colorscheme = { "typograph" } } }
 
 local packages = {
-	"wbthomason/packer.nvim",
-	"junegunn/vim-easy-align",
-	"stevearc/dressing.nvim",
-	"nvim-lua/plenary.nvim",
-	"elixir-editors/vim-elixir",
-	"chrisgrieser/nvim-genghis",
-	{ "yorickpeterse/nvim-pqf", config = true },
-	{ "numToStr/Comment.nvim", config = true },
-	{ "m4xshen/autoclose.nvim", config = true },
-	{ "akinsho/git-conflict.nvim", config = true },
-	{
-		"duane9/nvim-rg",
-		keys = { { "<leader>s", "<cmd>Rg<cr>", silent = true } },
-	},
-	{
-		"ibhagwan/fzf-lua",
-		config = true,
-		keys = {
-			{ "<c-p>", [[<cmd>lua require('fzf-lua').files()<CR>]], silent = true },
-			{ "<c-y>", [[<cmd>lua require('fzf-lua').resume()<CR>]], silent = true },
-			{ "<c-k>", [[<cmd>lua require('fzf-lua').commands()<CR>]], silent = true },
-			{ "<c-.>", [[<cmd>lua require('fzf-lua').grep_cword()<CR>]], silent = true },
-			{ "<c-g>", [[<cmd>lua require('fzf-lua').live_grep()<CR>]], silent = true },
-			{ "<c-\\>", [[<cmd>lua require('fzf-lua').oldfiles()<CR>]], silent = true },
-			{ "<C-x><C-f>", [[<cmd>lua require("fzf-lua").complete_path()<CR>]], silent = true, mode = "i" },
-		},
-	},
+  "wbthomason/packer.nvim",
+  "junegunn/vim-easy-align",
+  "stevearc/dressing.nvim",
+  "nvim-lua/plenary.nvim",
+  "elixir-editors/vim-elixir",
+  "chrisgrieser/nvim-genghis",
+  { "yorickpeterse/nvim-pqf",    config = true },
+  { "numToStr/Comment.nvim",     config = true },
+  { "m4xshen/autoclose.nvim",    config = true },
+  { "akinsho/git-conflict.nvim", config = true },
+  {
+    "duane9/nvim-rg",
+    keys = { { "<leader>s", "<cmd>Rg<cr>", silent = true } },
+  },
+  {
+    "ibhagwan/fzf-lua",
+    config = true,
+    keys = {
+      { "<c-p>",      [[<cmd>lua require('fzf-lua').files()<CR>]],         silent = true },
+      { "<c-y>",      [[<cmd>lua require('fzf-lua').resume()<CR>]],        silent = true },
+      { "<c-k>",      [[<cmd>lua require('fzf-lua').commands()<CR>]],      silent = true },
+      { "<c-.>",      [[<cmd>lua require('fzf-lua').grep_cword()<CR>]],    silent = true },
+      { "<c-g>",      [[<cmd>lua require('fzf-lua').live_grep()<CR>]],     silent = true },
+      { "<c-\\>",     [[<cmd>lua require('fzf-lua').oldfiles()<CR>]],      silent = true },
+      { "<C-x><C-f>", [[<cmd>lua require("fzf-lua").complete_path()<CR>]], silent = true, mode = "i" },
+    },
+  },
 
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-			"WhoIsSethDaniel/mason-tool-installer.nvim",
-		},
-		config = function()
-			local lspconfig = require("lspconfig")
-			require("mason").setup({})
-			require("mason-lspconfig").setup()
-			require("mason-lspconfig").setup_handlers({
-				function(server)
-					lspconfig[server].setup({})
-				end,
-			})
-			require("mason-tool-installer").setup({
-				ensure_installed = { "black", "stylua", "shellcheck", "eslint_d", "shfmt" },
-			})
-		end,
-	},
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function()
+      local lspconfig = require("lspconfig")
+      require("mason").setup({})
+      require("mason-lspconfig").setup()
+      require("mason-lspconfig").setup_handlers({
+        function(server) lspconfig[server].setup({}) end,
+      })
+      require("mason-tool-installer").setup({
+        ensure_installed = { "black", "stylua", "shellcheck", "eslint_d", "shfmt" },
+      })
+    end,
+  },
 
-	{
-		"dense-analysis/ale",
-		init = function()
-			vim.g.ale_disable_lsp = 1
-			vim.g.ale_use_neovim_diagnostics_api = 1
-			vim.g.ale_virtualtext_cursor = 0
-			vim.g.ale_fixers = { lua = { "stylua" }, python = "black", ruby = "rubocop" }
-		end,
-		keys = { { "g=", "<cmd>ALEFix<cr>", silent = true } },
-	},
+  {
+    "dense-analysis/ale",
+    init = function()
+      vim.g.ale_disable_lsp = 1
+      vim.g.ale_use_neovim_diagnostics_api = 1
+      vim.g.ale_virtualtext_cursor = 0
+      vim.g.ale_fixers = { lua = { "stylua" }, python = { "black" }, ruby = { "rubocop" } }
+    end,
+    keys = { { "g=", vim.cmd.ALEFix, silent = true } },
+  },
 
-	{
-		"lewis6991/gitsigns.nvim",
+  {
+    "lewis6991/gitsigns.nvim",
     cmd = "Gitsigns",
-		config = true,
-		keys = {
-			{ "<leader>gx", ":Gitsigns reset_hunk<cr>", silent = true },
-			{ "<leader>gb", ":Gitsigns blame_line<cr>", silent = true },
-			{ "<leader>gs", ":Gitsigns stage_hunk<cr>", silent = true },
-			{ "<leader>gS", ":Gitsigns stage_buffer<cr>", silent = true },
-			{ "<leader>gu", ":Gitsigns undo_stage_hunk<cr>", silent = true },
-		},
-	},
+    config = true,
+    keys = {
+      { "<leader>gx", ":Gitsigns reset_hunk<cr>",      silent = true },
+      { "<leader>gb", ":Gitsigns blame_line<cr>",      silent = true },
+      { "<leader>gs", ":Gitsigns stage_hunk<cr>",      silent = true },
+      { "<leader>gS", ":Gitsigns stage_buffer<cr>",    silent = true },
+      { "<leader>gu", ":Gitsigns undo_stage_hunk<cr>", silent = true },
+    },
+  },
 }
 
 lazy.setup(packages, opts)
--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -5,8 +5,6 @@ vim.g.maplocalleader = ","
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 20
 vim.g.netrw_keepdir = 0
-vim.g.netrw_localrmdir = "rm -r"
-vim.g.netrw_localcopycmd = "cp -r"
 vim.g.netrw_bufsettings = "noma nomod nu nobl nowrap ro"
 vim.o.exrc = true
 vim.o.title = true
@@ -30,43 +28,47 @@ vim.opt.statusline = [[ ]]
 vim.o.completeopt = "menu"
 
 -- Set Keymaps
-local keymap = vim.keymap.set
 local autocmd = vim.api.nvim_create_autocmd
-local options = { remap = true, silent = true }
-keymap('n', ';w', vim.cmd.update, options)
-keymap('n', '<c-w>q', function()
-      local multiple_windows, _ = pcall(vim.cmd, "close")
-      if not multiple_windows then
-        vim.cmd("bd")
-      end
-    end
-, options)
-keymap('n', '<C-w>t', vim.cmd.tabnew, options)
-keymap('i', '<C-o>', '<C-x><C-o>', options)
-keymap('i', '<C-f>', '<C-x><C-f>', options)
-keymap('n', '<Tab>', vim.cmd.tabnext, options)
-keymap('n', '<S-Tab>', vim.cmd.tabprevious, options)
-keymap('v', '<', '<gv', options)
-keymap('v', '>', '>gv', options)
-keymap('n', '<C-t><C-t>', ':tabnew | terminal<cr>', options)
-keymap('n', '<C-t>v', ':vsplit | terminal<cr>', options)
-keymap('n', '<C-t>s', ':split | terminal<cr>', options)
-keymap('i', '<Tab>', function() return vim.fn.pumvisible() == 1 and "<C-n>" or "<Tab>" end, { expr = true })
-keymap('n', '-', vim.cmd.Ex, options)
-keymap('n', '<leader>v', vim.cmd.Lex, options)
-autocmd('LspAttach', {
+local keymap = function(mode, cmd, keys, options)
+  local opts = options or { remap = true, silent = true }
+  vim.keymap.set(mode, cmd, keys, opts)
+end
+keymap("n", ";w", vim.cmd.update)
+keymap("n", "<c-w>q", function()
+  local multiple_windows, _ = pcall(vim.cmd, "close")
+  if not multiple_windows then
+    vim.cmd("bd")
+  end
+end)
+keymap("n", "<C-w>t", vim.cmd.tabnew)
+keymap("i", "<C-o>", "<C-x><C-o>")
+keymap("i", "<C-f>", "<C-x><C-f>")
+keymap("n", "<Tab>", vim.cmd.tabnext)
+keymap("n", "<S-Tab>", vim.cmd.tabprevious)
+keymap("v", "<", "<gv")
+keymap("v", ">", ">gv")
+keymap("n", "<C-t><C-t>", ":tabnew | terminal<cr>")
+keymap("n", "<C-t>v", ":vsplit | terminal<cr>")
+keymap("n", "<C-t>s", ":split | terminal<cr>")
+keymap("i", "<Tab>", function()
+  return vim.fn.pumvisible() == 1 and "<C-n>" or "<Tab>"
+end, { expr = true })
+keymap("n", "-", vim.cmd.Ex)
+keymap("n", "<leader>v", vim.cmd.Lex)
+autocmd("LspAttach", {
   callback = function(ev)
     local opts = { buffer = ev.buf, silent = true, remap = true }
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-    keymap({ 'n', 'v' }, 'gq', vim.lsp.buf.format, options)
-    keymap('n', 'gd', vim.lsp.buf.definition, opts)
-    keymap('n', 'gr', vim.lsp.buf.references, opts)
-    keymap('n', 'K', vim.lsp.buf.hover, opts)
-    keymap('n', '<leader>rn', vim.lsp.buf.rename, opts)
-    keymap('n', '<leader><leader>', vim.lsp.buf.code_action, opts)
+    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+    keymap({ "n", "v" }, "g=", vim.lsp.buf.format)
+    keymap("n", "gd", vim.lsp.buf.definition, opts)
+    keymap("n", "gr", vim.lsp.buf.references, opts)
+    keymap("n", "K", vim.lsp.buf.hover, opts)
+    keymap("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+    keymap("n", "<leader>lr", vim.lsp.buf.rename, opts)
+    keymap("n", "<leader>la", vim.lsp.buf.code_action, opts)
   end,
 })
-keymap('n', '[d', vim.diagnostic.goto_prev, options)
-keymap('n', ']d', vim.diagnostic.goto_next, options)
-keymap('n', 'gl', function() vim.diagnostic.open_float({ scope = 'line' }) end, options)
-keymap('n', '<leader>q', vim.diagnostic.setqflist, options)
+keymap("n", "[d", vim.diagnostic.goto_prev)
+keymap("n", "]d", vim.diagnostic.goto_next)
+keymap("n", "gl", function() vim.diagnostic.open_float({ scope = "line" }) end)
+keymap("n", "<leader>q", vim.diagnostic.setqflist)
