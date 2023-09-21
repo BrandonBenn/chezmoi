a42800294baa3a3137e7598840df9e39db640f83
Brandon Duval Benn <me@brandonbenn.xyz>
2023-09-21 08:07:05 +0800

    Update .config/nvim/after/plugin/lazy.lua

--- private_dot_config/nvim/after/plugin/lazy.lua
+++ private_dot_config/nvim/after/plugin/lazy.lua
@@ -1,141 +1,106 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	return
-end
+if not vim.loop.fs_stat(lazypath) then return end
 vim.opt.rtp:prepend(lazypath)
 local lazy = require("lazy")
 
 local opts = { install = { missing = true, colorscheme = { "typograph" } } }
 
 local packages = {
-	"wbthomason/packer.nvim",
-	"nvim-lua/plenary.nvim",
-	"stevearc/dressing.nvim",
-	"justinmk/vim-dirvish",
-	"tpope/vim-unimpaired",
-	"chrisgrieser/nvim-genghis",
-	{ "elixir-editors/vim-elixir", ft = "elixir" },
-	{ "yorickpeterse/nvim-pqf", config = true },
-	{ "numToStr/Comment.nvim", config = true },
-	{ "m4xshen/autoclose.nvim", config = true },
-	{ "akinsho/git-conflict.nvim", config = true },
-	{
-		"lewis6991/gitsigns.nvim",
-		lazy = false,
-		config = true,
-		keys = {
-			{ "<leader>gx", ":Gitsigns reset_hunk<cr>", silent = true },
-			{ "<leader>gb", ":Gitsigns blame_line<cr>", silent = true },
-			{ "<leader>gs", ":Gitsigns stage_hunk<cr>", silent = true },
-			{ "<leader>gS", ":Gitsigns stage_buffer<cr>", silent = true },
-			{ "<leader>gu", ":Gitsigns undo_stage_hunk<cr>", silent = true },
-		},
-	},
+  "wbthomason/packer.nvim",
+  "nvim-lua/plenary.nvim",
+  "stevearc/dressing.nvim",
+  "chrisgrieser/nvim-genghis",
+  { "elixir-editors/vim-elixir", ft = "elixir" },
+  { "yorickpeterse/nvim-pqf",    config = true },
+  { "numToStr/Comment.nvim",     config = true },
+  { "m4xshen/autoclose.nvim",    config = true },
+  { "akinsho/git-conflict.nvim", config = true },
 
-	{
-		"ibhagwan/fzf-lua",
-		cmd = { "FzfLua" },
-		config = true,
-		opts = { winopts = { height = 0.30, width = 0.4, preview = { hidden = "hidden" } } },
-		keys = {
-			{
-				"<leader>ff",
-				function()
-					require("fzf-lua").files()
-				end,
-				silent = true,
-			},
-			{
-				"<leader>fo",
-				function()
-					require("fzf-lua").oldfiles()
-				end,
-				silent = true,
-			},
-			{
-				"<leader>fk",
-				function()
-					require("fzf-lua").commands()
-				end,
-				silent = true,
-			},
-			{
-				"<leader>fw",
-				function()
-					require("fzf-lua").grep_cword()
-				end,
-				silent = true,
-			},
-			{
-				"<leader>fb",
-				function()
-					require("fzf-lua").buffers()
-				end,
-				silent = true,
-			},
-			{
-				"<leader>fg",
-				function()
-					require("fzf-lua").live_grep({ continue_last_search = true })
-				end,
-				silent = true,
-			},
-			{
-				"<C-x><C-f>",
-				function()
-					require("fzf-lua").complete_path()
-				end,
-				silent = true,
-			},
-		},
-	},
+  {
+    "stevearc/oil.nvim",
+    config = true,
+    keys = { { "-", vim.cmd.Oil, silent = true, desc = "Open parent directory" } },
+  },
 
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-			"WhoIsSethDaniel/mason-tool-installer.nvim",
-			"creativenull/efmls-configs-nvim",
-		},
-		config = function()
-			local lspconfig = require("lspconfig")
-			require("mason").setup({})
-			require("mason-lspconfig").setup()
-			require("mason-lspconfig").setup_handlers({
-				function(server)
-					lspconfig[server].setup({})
-				end,
-			})
+  {
+    "lewis6991/gitsigns.nvim",
+    lazy = false,
+    config = true,
+    keys = {
+      { "<leader>gx", ":Gitsigns reset_hunk<cr>",      silent = true, desc = "Git reset hunk" },
+      { "<leader>gb", ":Gitsigns blame_line<cr>",      silent = true, desc = "Git blame line" },
+      { "<leader>gs", ":Gitsigns stage_hunk<cr>",      silent = true, desc = "Git stage hunk" },
+      { "<leader>gS", ":Gitsigns stage_buffer<cr>",    silent = true, desc = "Git stage buffer" },
+      { "<leader>gu", ":Gitsigns undo_stage_hunk<cr>", silent = true, desc = "Git undo stage buffer" },
+    },
+  },
 
-			local ensure_installed = { "efm", "black", "stylua", "shellcheck", "eslint_d", "shfmt" }
-			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+  {
+    "ibhagwan/fzf-lua",
+    cmd = { "FzfLua" },
+    config = true,
+    opts = { winopts = { height = 0.30, width = 0.4, preview = { hidden = "hidden" } } },
+    keys = {
+      { "<leader>ff", "<cmd>FzfLua files<cr>",         silent = true, desc = "Find Files" },
+      { "<leader>fo", "<cmd>FzfLua oldfiles<cr>",      silent = true, desc = "Find Old fles" },
+      { "<leader>fk", "<cmd>FzfLua commands<cr>",      silent = true, desc = "Find commands" },
+      { "<leader>fb", "<cmd>FzfLua buffers<cr>",       silent = true, desc = "Find buffers" },
+      { "<leader>fw", "<cmd>FzfLua grep_cword<cr>",    silent = true, desc = "Grep Word" },
+      { "<C-x><C-f>", "<cmd>FzfLua complete_path<cr>", silent = true, desc = "Complete Path" },
+      {
+        "<leader>fg",
+        function()
+          require("fzf-lua").live_grep({ continue_last_search = true })
+        end,
+        silent = true,
+        desc = "Live Grep",
+      },
+    },
+  },
 
-			local languages = require("efmls-configs.defaults").languages()
-			languages = vim.tbl_extend("force", languages, {
-				python = { require("efmls-configs.formatters.black") },
-				html = { require("efmls-configs.formatters.prettier") },
-				sh = { require("efmls-configs.formatters.shfmt"), require("efmls-configs.linters.shellcheck") },
-			})
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function()
+      local lspconfig = require("lspconfig")
+      require("mason").setup({})
+      require("mason-lspconfig").setup()
+      require("mason-lspconfig").setup_handlers({
+        function(server)
+          lspconfig[server].setup({})
+        end,
+      })
 
-			lspconfig.efm.setup({
-				settings = {
-					rootMarkers = { ".git/", "manage.py", "Gemfile" },
-					languages = languages,
-				},
-				filetypes = vim.tbl_keys(languages),
-				init_options = { documentFormatting = true, documentRangeFormatting = true },
-			})
-		end,
-	},
+      require("mason-tool-installer").setup({
+        ensure_installed = { "ruff", "stylua", "shellcheck", "eslint_d", "prettier" },
+      })
+    end,
+  },
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		config = true,
-		opts = {
-			ensure_installed = { "go", "html", "javascript", "python", "ruby", "sql", "vue" },
-		},
-	},
+  {
+    "dense-analysis/ale",
+    init = function()
+      vim.g.ale_fix_on_save = 1
+      vim.g.ale_use_neovim_diagnostics_api = 1
+      vim.g.ale_fixers = {
+        python = { "ruff" },
+        javascript = { "prettier", "eslint_d" },
+        sh = { "shellcheck" }
+      }
+    end
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = true,
+    opts = {
+      ensure_installed = { "go", "html", "javascript", "python", "ruby", "sql", "vue" },
+    },
+  },
 }
 
 lazy.setup(packages, opts)
