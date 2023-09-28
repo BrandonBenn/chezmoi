4d4318247ced676ee05ebd5b2780a25e0a6f930b
Brandon Duval Benn <me@brandonbenn.xyz>
2023-09-28 08:30:20 +0800

    Update .config/nvim/after/plugin/lazy.lua

--- private_dot_config/nvim/after/plugin/lazy.lua
+++ private_dot_config/nvim/after/plugin/lazy.lua
@@ -10,13 +10,19 @@ local packages = {
   "nvim-lua/plenary.nvim",
   "stevearc/dressing.nvim",
   "chrisgrieser/nvim-genghis",
+  "tpope/vim-fugitive",
   { "elixir-editors/vim-elixir", ft = "elixir" },
   { "yorickpeterse/nvim-pqf",    config = true },
   { "numToStr/Comment.nvim",     config = true },
   { "m4xshen/autoclose.nvim",    config = true },
+  {
+    "Exafunction/codeium.vim",
+    event = "BufEnter",
+  },
 
   {
     'nvim-telescope/telescope.nvim',
+    cmd = { 'Telescope' },
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       local action_layout = require("telescope.actions.layout")
@@ -45,6 +51,7 @@ local packages = {
     keys = {
       { '<leader>fb', '<cmd>Telescope buffers<cr>',                   silent = true },
       { '<leader>ff', '<cmd>Telescope find_files<cr>',                silent = true },
+      { '<leader>fo', '<cmd>Telescope oldfiles<cr>',                  silent = true },
       { '<leader>fg', '<cmd>Telescope live_grep<cr>',                 silent = true },
       { '<leader>fh', '<cmd>Telescope help_tags<cr>',                 silent = true },
       { '<leader>fs', '<cmd>Telescope current_buffer_fuzzy_find<cr>', silent = true },
@@ -55,29 +62,6 @@ local packages = {
     "stevearc/oil.nvim",
     config = true,
     keys = { { "-", vim.cmd.Oil, silent = true, desc = "Open parent directory" } },
-  },
-
-  {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "sindrets/diffview.nvim",
-    },
-    config = true,
-    keys = { { "<leader>gg", vim.cmd.Neogit, silent = true, desc = "Git status" } },
-  },
-
-  {
-    "lewis6991/gitsigns.nvim",
-    lazy = false,
-    config = true,
-    keys = {
-      { "<leader>gx", ":Gitsigns reset_hunk<cr>",      silent = true, desc = "Git reset hunk" },
-      { "<leader>gb", ":Gitsigns blame_line<cr>",      silent = true, desc = "Git blame line" },
-      { "<leader>gs", ":Gitsigns stage_hunk<cr>",      silent = true, desc = "Git stage hunk" },
-      { "<leader>gS", ":Gitsigns stage_buffer<cr>",    silent = true, desc = "Git stage buffer" },
-      { "<leader>gu", ":Gitsigns undo_stage_hunk<cr>", silent = true, desc = "Git undo stage buffer" },
-    },
   },
 
   {
@@ -120,7 +104,7 @@ local packages = {
     "nvim-treesitter/nvim-treesitter",
     config = true,
     opts = {
-      ensure_installed = { "go", "html", "javascript", "python", "ruby", "sql", "vue" },
+      ensure_installed = { "go", "html", "javascript", "python", "ruby", "sql", "vue", "lua" },
     },
   },
 }
