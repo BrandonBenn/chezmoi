98e209f2deb1cd55d21ef753c221db85141a81be
Brandon Duval Benn <me@brandonbenn.xyz>
2023-09-28 08:42:42 +0800

    Update .config/nvim/after/plugin/lazy.lua

--- private_dot_config/nvim/after/plugin/lazy.lua
+++ private_dot_config/nvim/after/plugin/lazy.lua
@@ -10,14 +10,21 @@ local packages = {
   "nvim-lua/plenary.nvim",
   "stevearc/dressing.nvim",
   "chrisgrieser/nvim-genghis",
-  "tpope/vim-fugitive",
+
   { "elixir-editors/vim-elixir", ft = "elixir" },
   { "yorickpeterse/nvim-pqf",    config = true },
   { "numToStr/Comment.nvim",     config = true },
   { "m4xshen/autoclose.nvim",    config = true },
+
   {
     "Exafunction/codeium.vim",
     event = "BufEnter",
+  },
+
+  {
+    "tpope/vim-fugitive",
+    cmd = "Git",
+    keys = { { '<leader>gg', '<cmd>Git<cr>', silent = true, desc = "[G]it" } },
   },
 
   {
@@ -26,7 +33,6 @@ local packages = {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       local action_layout = require("telescope.actions.layout")
-      local actions = require("telescope.actions")
       require('telescope').setup {
         defaults = require('telescope.themes').get_dropdown({
           mappings = {
@@ -36,7 +42,6 @@ local packages = {
             },
             i = {
               ["<C-u>"] = false,
-              ["<esc>"] = actions.close,
               ["<M-p>"] = action_layout.toggle_preview,
             },
           }
@@ -49,12 +54,15 @@ local packages = {
       }
     end,
     keys = {
-      { '<leader>fb', '<cmd>Telescope buffers<cr>',                   silent = true },
-      { '<leader>ff', '<cmd>Telescope find_files<cr>',                silent = true },
-      { '<leader>fo', '<cmd>Telescope oldfiles<cr>',                  silent = true },
-      { '<leader>fg', '<cmd>Telescope live_grep<cr>',                 silent = true },
-      { '<leader>fh', '<cmd>Telescope help_tags<cr>',                 silent = true },
-      { '<leader>fs', '<cmd>Telescope current_buffer_fuzzy_find<cr>', silent = true },
+      { '<leader>fb', '<cmd>Telescope buffers<cr>',                   silent = true, desc = "[F]ind [B]uffer" },
+      { '<leader>fr', '<cmd>Telescope resume<cr>',                    silent = true, desc = "[F]ind [R]esume" },
+      { '<leader>fp', '<cmd>Telescope git_files<cr>',                 silent = true, desc = "[F]ind [P]roject Files" },
+      { '<leader>ff', '<cmd>Telescope find_files<cr>',                silent = true, desc = "[F]ind [F]iles" },
+      { '<leader>fo', '<cmd>Telescope oldfiles<cr>',                  silent = true, desc = "[F]ind [O]ld Files" },
+      { '<leader>fw', '<cmd>Telescope grep_string<cr>',               silent = true, desc = "[F]ind [W]ord" },
+      { '<leader>fg', '<cmd>Telescope live_grep<cr>',                 silent = true, desc = "[F]ind [G]rep" },
+      { '<leader>fh', '<cmd>Telescope help_tags<cr>',                 silent = true, desc = "[F]ind [H]elp" },
+      { '<leader>fs', '<cmd>Telescope current_buffer_fuzzy_find<cr>', silent = true, desc = "[F]ind [S]earch" },
     },
   },
 
@@ -76,13 +84,11 @@ local packages = {
       require("mason").setup({})
       require("mason-lspconfig").setup()
       require("mason-lspconfig").setup_handlers({
-        function(server)
-          lspconfig[server].setup({})
-        end,
+        function(server) lspconfig[server].setup({}) end,
       })
 
       require("mason-tool-installer").setup({
-        ensure_installed = { "ruff", "stylua", "shellcheck", "eslint_d", "prettier" },
+        ensure_installed = { "ruff", "stylua", "shellcheck", "eslint_d", "prettier", "shfmt" },
       })
     end,
   },
@@ -95,7 +101,7 @@ local packages = {
       vim.g.ale_fixers = {
         python = { "ruff" },
         javascript = { "prettier", "eslint_d" },
-        sh = { "shellcheck" }
+        sh = { "shellcheck", "shfmt" }
       }
     end
   },
