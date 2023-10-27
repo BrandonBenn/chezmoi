e76cbe9ac11836ed15cf06be72ba73758942dc2a
Brandon Duval Benn <me@brandonbenn.xyz>
2023-10-27 19:39:50 +0800

    Update .config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -4,6 +4,7 @@ return {
   window_decorations = "INTEGRATED_BUTTONS|RESIZE",
   disable_default_key_bindings = true,
   font = wezterm.font('Iosevka Nerd Font'),
+  window_background_opacity = 0.9,
   color_scheme = 'Calamity',
   keys = require("keys"),
 }
